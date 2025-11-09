e2d9d9dcd4d7db3799c61a7288d962907cb9b313
pespen <pederespen@gmail.com>
2025-11-09 13:35:29 +0100

    lint

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/budgetbuddy' : '',
+      base: process.env.NODE_ENV === "production" ? "/budgetbuddy" : "",
     },
   },
 };
